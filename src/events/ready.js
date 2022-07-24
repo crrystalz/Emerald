6de7963cda628b434f52837345dee3d4543e9761
crystalz <lichupatnaik@gmail.com>
2022-07-24 12:24:47 -0700

    Oscillating status :D

--- src/events/ready.js
+++ src/events/ready.js
@@ -2,7 +2,23 @@ module.exports = {
     name: 'ready',
     once: true,
     async execute(client) {
+        const activities = [
+            "made by crrystalz#0001",
+            "/help"
+        ]
         console.log('Emerald is online!');
-        client.user.setActivity(" made by crrystalz#0001")
+        var index = 0;
+        setInterval(() => {
+            const newActivity = activities[index];
+            client.user.setActivity(newActivity);
+
+            if (index == 0) {
+                index = 1;
+            }
+            else {
+                index = 0;
+            }
+
+        }, 5000);
     }
 }
